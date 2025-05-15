b081d8c1d72dd08b0784978a37018da90bb65be9
Yeray2 <101354040+Yeray2@users.noreply.github.com>
2025-05-15 17:41:04 +0200

    p`lauer

--- Assets/Scripts/Shots/ShotGun.cs
+++ Assets/Scripts/Shots/ShotGun.cs
@@ -1,44 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Player; // Para usar Player.Surface
 
-public class ShotGun : MonoBehaviour {
+public class ShotGun : MonoBehaviour
+{
     float speed = 8f;
 
-	
-	// Update is called once per frame
-	void Update ()
+    Vector2 direction = Vector2.up;
+
+    void Update()
     {
-        if(transform.rotation.z == 0)
-        {
-            transform.position += Vector3.up * Time.deltaTime * speed;
-        }
-        else if(transform.rotation.z < 0)
-        {
-            transform.position += new Vector3(.1f,1) * Time.deltaTime * speed;
-        }
-        else
-        {
-            transform.position += new Vector3(-.1f, 1) * Time.deltaTime * speed;
-        }
+        transform.position += (Vector3)(direction.normalized * speed * Time.deltaTime);
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "zapaia")
+        if (collision.CompareTag("zapaia"))
         {
-          
+            // Lógica opcional
         }
-        if (collision.gameObject.tag == "ball")
+        else if (collision.CompareTag("ball"))
         {
-            collision.gameObject.GetComponent<Ball>().Split();
-            
+            collision.GetComponent<Ball>().Split();
         }
-        if (collision.gameObject.tag == "Hexagon")
+        else if (collision.CompareTag("Hexagon"))
         {
-            collision.gameObject.GetComponent<Hexagon>().Split();
-           
+            collision.GetComponent<Hexagon>().Split();
         }
+
         Destroy(gameObject);
         ShootManager.shm.DestroyShot();
+    }
+
+    public void SetSurface(Surface surface)
+    {
+        switch (surface)
+        {
+            case Surface.Lurra:
+                direction = Vector2.up;
+                break;
+            case Surface.Zapaia:
+                direction = Vector2.down;
+                break;
+            case Surface.Ezkerra:
+                direction = Vector2.right;
+                break;
+            case Surface.Eskubi:
+                direction = Vector2.left;
+                break;
+        }
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle - 90f);
     }
 }
